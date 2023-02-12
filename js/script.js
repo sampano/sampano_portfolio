ed808c8a878b7e4ac1d463b96435e175db551a29
Samuel Pano <spano@ph.drakeintl.com>
2023-02-12 19:54:45 +0800

    added styles and animation

--- js/script.js
+++ js/script.js
@@ -2,18 +2,6 @@ const headerLists = document.querySelectorAll(".header-list");
 const contentLists = document.querySelectorAll(
   ".portfolio-experience__content"
 );
-// for (let headerList of headerLists) {
-//   headerList.addEventListener("click", showTabContent);
-//   function showTabContent(e) {
-//     if (headerList) {
-//       console.log(e);
-//       headerList.classList.toggle("active");
-//     } else {
-//       headerList.classList.remove("active");
-//     }
-//   }
-// }
-
 headerLists.forEach(function (item, index) {
   item.addEventListener(
     "click",
@@ -30,3 +18,41 @@ headerLists.forEach(function (item, index) {
     false
   );
 });
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElements2 = document.querySelectorAll(".hidden2");
+hiddenElements2.forEach((el) => observer.observe(el));
+
+//BURGER MENU
+const burgerMenu = document.querySelector(".burger-open");
+const navItems = document.querySelector(".nav-items");
+
+const showNavItems = () => {
+  for (let burgerClass of burgerMenu.classList) {
+    if (burgerClass === "fa-burger") {
+      burgerMenu.classList.remove("fa-burger");
+      burgerMenu.classList.add("fa-x");
+      document.body.classList.add("sider");
+      navItems.classList.add("active");
+      document.body.classList.remove("sider-close");
+    } else if (burgerClass === "fa-x") {
+      burgerMenu.classList.remove("fa-x");
+      burgerMenu.classList.add("fa-burger");
+      document.body.classList.remove("sider");
+      document.body.classList.add("sider-close");
+      navItems.classList.remove("active");
+    }
+  }
+};
+
+burgerMenu.addEventListener("click", showNavItems);
